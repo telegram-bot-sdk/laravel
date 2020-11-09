e28333de638a36e1cbbbe0fa63ad15a93a683a5e
Jonathan Williamson <jonnywilliamson@gmail.com>
2020-11-09 14:41:33 +0000

    Install beyondcode/expose as a dev dependency. Only show expose command in NON production environments.

--- src/TelegramServiceProvider.php
+++ src/TelegramServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Telegram\Bot\Laravel;
 
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 use Telegram\Bot\Api;
@@ -94,16 +95,22 @@ class TelegramServiceProvider extends ServiceProvider
      */
     protected function registerCommands(): void
     {
-        if ($this->app->runningInConsole()) {
-            $this->commands([
+        $commands = collect(
+            [
                 Console\Command\CommandListCommand::class,
                 Console\Command\CommandMakeCommand::class,
                 Console\Command\CommandRegisterCommand::class,
                 Console\Webhook\WebhookInfoCommand::class,
                 Console\Webhook\WebhookRemoveCommand::class,
                 Console\Webhook\WebhookRegisterCommand::class,
-                Console\Webhook\WebhookExposeCommand::class,
-            ]);
+            ])
+            ->unless($this->app->environment('production'), function (Collection $collection) {
+                    return $collection->merge([Console\Webhook\WebhookExposeCommand::class]);
+            })
+            ->toArray();
+
+        if ($this->app->runningInConsole()) {
+            $this->commands($commands);
         }
     }
 
